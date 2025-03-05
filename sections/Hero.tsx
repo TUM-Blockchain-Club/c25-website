f89c67f9e5424776302dd40ae71708d52ce1c8d0
ge95bid <ge95bid@tum.de>
2025-03-06 00:20:17 +0100

    add event date display to Hero section

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -11,6 +11,11 @@ const Hero = () => {
             <div className="relative max-w-[80vw] xl:max-w-[50vw] min-h-[150px] w-[400px] h-[175px] xs:mt-10 sm:w-[500px] sm:h-[200px] lg:w-[700px] lg:h-[350px]">
               <Image src={confLogo} alt="TUM Blockchain Conference 24" fill />
             </div>
+            <div className={"flex flex-col items-center gap-1"}>
+              <Text as={"p"} textType={"sub_title"} className="text-center">
+                September 11<sup>th</sup> - 12<sup>th</sup> 2025
+              </Text>
+            </div>
             {/* <div className={"flex flex-col items-center gap-1"}>
               <Text as={"p"} textType={"sub_title"} className="text-center">
                 Deutsches Museum, Munich
