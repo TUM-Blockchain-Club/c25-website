f428de9366ffe6b4632d8ed555638916d3ee1f1d
ge95bid <ge95bid@tum.de>
2025-09-10 09:51:37 +0200

    fix: now page

--- app/now/page.tsx
+++ app/now/page.tsx
@@ -1,12 +1,16 @@
 import { useSession } from "@/hooks/useSession";
 import Now from "@/app/now/now";
+import { fetchSessions } from "@/components/service/contentStrapi_static";
+import { fetchSpeakers } from "@/components/service/contentStrapi";
 
 export default async function NowPage() {
-  const sessions = await useSession();
+  const sessions = await fetchSessions();
+  const speakers = await fetchSpeakers();
 
   return (
     <Now
       sessions={sessions}
+      speakers={speakers}
       simulatedDate={new Date("2024-09-12T09:00:00+02:00")}
     />
   );
